0794dd816a540394c389d313b08959b0fdeb9676
er-ravi-srivastava <raviprakashshrivastav7@gmail.com>
2025-02-22 17:23:59 +0530

    made some changes

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -3,11 +3,9 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
 
-
 # Initialize WebDriver
-driver = webdriver.Chrome()
+driver = webdriver.Firefox()
 driver.get("https://www.google.com")
-
 
 # Search for a query
 search_box = driver.find_element(By.NAME, "q")
@@ -15,12 +13,11 @@ search_box.send_keys("Selenium Python")
 search_box.send_keys(Keys.RETURN)
 
 # Wait for results to load
-time.sleep(6)
+time.sleep(2)
 
 # Extract search result titles
 results = driver.find_elements(By.CSS_SELECTOR, "h3")
 for result in results:
-    print(f"Found {len(results)} results")
     print(result.text)
 
 # Close the driver
